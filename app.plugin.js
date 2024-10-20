281e6ca8949c4cf1cc99d798fd40cf1610e2a294
rieg-ec <rieg.10199@gmail.com>
2024-10-20 13:22:39 -0600

    feat: add expo plugin to increase device availability

---
+++ app.plugin.js
@@ -0,0 +1,51 @@
+const { withAndroidManifest } = require('@expo/config-plugins');
+
+module.exports = function withPhotoPicker(_config) {
+  return withAndroidManifest(_config, async (config) => {
+    const androidManifest = config.modResults.manifest;
+
+    // Define the service entry that we want to add to the manifest
+    const photoPickerService = {
+      service: [
+        {
+          '$': {
+            'android:name':
+              'com.google.android.gms.metadata.ModuleDependencies',
+            'android:enabled': 'false',
+            'android:exported': 'false',
+            'tools:ignore': 'MissingClass',
+          },
+          'intent-filter': [
+            {
+              action: [
+                {
+                  $: {
+                    'android:name':
+                      'com.google.android.gms.metadata.MODULE_DEPENDENCIES',
+                  },
+                },
+              ],
+            },
+          ],
+          'meta-data': [
+            {
+              $: {
+                'android:name': 'photopicker_activity:0:required',
+                'android:value': '',
+              },
+            },
+          ],
+        },
+      ],
+    };
+
+    // Ensure we only add the service if it doesn't already exist
+    if (!androidManifest.application[0].service) {
+      androidManifest.application[0].service = [];
+    }
+
+    androidManifest.application[0].service.push(photoPickerService);
+
+    return config;
+  });
+};
